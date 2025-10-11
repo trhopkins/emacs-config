7c45ca9ce1842b17859814cc9cff75d37e6c108b
Travis Reid Hopkins <trh.foss@protonmail.com>
2025-10-11 01:55:54 -0400

    fix: added Guile Geiser commands and some capture fixes in Org

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+; general Emacs settings
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+(set-face-attribute 'default nil :height 240)
+(load-theme 'wombat t)
+(setq-default org-enforce-todo-dependencies t)
+
+(setq custom-file "~/.emacs.d/customizations.el")
+
 ; Org Mode basic settings
 (require 'org)
 (define-key global-map "\C-ca" 'org-agenda)
@@ -15,50 +26,67 @@
 			 (concat org-directory "/leisure.org")
 			 (concat org-directory "/work.org")
 			 (concat org-directory "/code.org") ))
-;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;(setq org-capture-templates
-;      '(("w" "Work" entry (file+headline (concat org-directory "/work.org") "Tasks")
-;	 "** TODO %?\n  %U\n  %a"
-;	 :empty-lines 1)
-;	("c" "Code" entry (file+headline (concat org-directory "/code.org") "Tasks")
-;	 "** TODO %?\n  %U\n  %a"
-;	 :empty-lines 1)
-;	("h" "Chore" entry (file+headline (concat org-directory "/chores.org") "Tasks")
-;	 "** TODO %?\n  SCHEDULED: %t\n  %U"
-;	 :empty-lines 1)
-;	("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
-;	 "* %?\n  %U\n"
-;	 :empty-lines 1)
-;	))
 
 (setq org-capture-templates
-      `(("t" "Todo" entry (file+headline "~/Documents/org/chores.org" "Tasks")
-         "* TODO %?\n  %i\n  %a")
-        ("j" "Journal" entry (file+datetree ,(concat org-directory "/journal.org"))
-         "* %?\nEntered on %U\n  %i\n  %a") ))
+      '(("w" "Work" entry (file+headline (concat org-directory "/work.org") "Tasks")
+         "* TODO %?\n  %U\n  %a"
+         :empty-lines 1)
 
-; stolen from https://github.com/MooersLab/emacs-simple-init-org/blob/fb8b922e355cc3ce70c0e72d59028673f19cb43d/init.el#L1829C1-L1843C17
-(defun org-ask-location ()
-  (let* ((org-refile-targets '((nil :maxlevel . 9)))
-         (hd (condition-case nil
-                 (car (org-refile-get-location "Tag" nil t))
-               (error (car org-refile-history)))))
-    (goto-char (point-min))
-    (outline-next-heading)
-    (if (re-search-forward
-         (format org-complex-heading-regexp-format (regexp-quote hd))
-         nil t)
-        (goto-char (point-at-bol))
-      (goto-char (point-max))
-      (or (bolp) (insert "\n"))
-      (insert "* " hd "\n")))
-  (end-of-line))
+        ("c" "Code" entry (file+headline "~/Documents/org/code.org" "Tasks")
+         "* TODO %?\n  %U\n  %a"
+         :empty-lines 1)
 
-; general Emacs settings
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(setq inhibit-startup-screen t)
-(set-face-attribute 'default nil :height 240)
-(load-theme 'adwaita t)
-(setq-default org-enforce-todo-dependencies t)
+        ("h" "Chore" entry (file+headline "~/Documents/org/chores.org" "Tasks")
+         "* TODO %?\n  SCHEDULED: %t\n  %U"
+         :empty-lines 1)
+
+        ("l" "Leisure" entry (file+headline "~/Documents/org/leisure.org" "Activities")
+         "* %?\n  %U"
+         :empty-lines 1)
+
+        ("j" "Journal" entry (file+datetree "~/Documents/org/journal.org")
+         "* %?\n  Entered on %U\n"
+         :empty-lines 1)
+
+        ("n" "Note" entry (file+headline "~/Documents/org/notes.org" "Inbox")
+         "* %?\n  %U\n  %i\n  %a"
+         :empty-lines 1)))
+
+; steal from https://github.com/MooersLab/emacs-simple-init-org/blob/fb8b922e355cc3ce70c0e72d59028673f19cb43d/init.el#L1829C1-L1843C17?
+
+; Org Babel Geiser Guile setup
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((scheme . t)))
+
+;(setq geiser-default-implementation 'guile)
+
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(use-package geiser
+  :ensure t)
+
+(use-package geiser-guile
+  :ensure t
+  :after geiser
+  :config
+  (setq geiser-default-implementation 'guile)
+  (setq geiser-guile-binary "/usr/bin/guile3.0"))
+
+(use-package paredit
+  :ensure t
+  :hook ((emacs-lisp-mode . paredit-mode)
+         (lisp-mode . paredit-mode)
+         (scheme-mode . paredit-mode)
+         (geiser-repl-mode . paredit-mode))
+  :config
+  (define-key paredit-mode-map (kbd "C-j") nil))
+
+(add-hook 'geiser-repl-mode-hook
+          #'(lambda () (define-key paredit-mode-map (kbd "C-j") 'geiser-repl-maybe-send)))
